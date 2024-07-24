a5e5090bbcf2033e969b1d9724a29055eb064ab4
Zhining Li <zhiningli.dev@gmail.com>
2024-07-24 23:35:31 +0100

    Refactored mealplan page

--- src/components/MealPlanPage.js
+++ src/components/MealPlanPage.js
@@ -18,13 +18,19 @@ function MealPlanPage() {
       <div className='main-content'>
         <Sidebar />
         <div className='content'>
-          <button className='action-button' onClick={handleCreateNewMeals}>
-            Create New Meals
-          </button>
-          {showShoppingList && <ShoppingList />}
-          <button className='action-button' onClick={() => alert('Record Meals clicked!')}>
-            Record Meals You Have Eaten
-          </button>
+          <div className='meal-plan-section'>
+            <h2>Ongoing Meal Plan</h2>
+            <p>You have no meal planned today</p>
+          </div>
+          <div className='action-section'>
+            <button className='action-button' onClick={handleCreateNewMeals}>
+              Create New Meals
+            </button>
+            {showShoppingList && <ShoppingList />}
+            <button className='action-button' onClick={() => alert('Record Meals clicked!')}>
+              Record Meals You Have Eaten
+            </button>
+          </div>
         </div>
       </div>
     </div>
