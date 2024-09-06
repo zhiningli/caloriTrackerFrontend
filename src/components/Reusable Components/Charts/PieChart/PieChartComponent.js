012764a631ca4cfc70b43e4fb41c0955f662305c
Zhining Li <zhiningli.dev@gmail.com>
2024-09-06 07:35:45 +0100

    Added pieChart animation and label to the MealPlanDashboard

--- src/components/Reusable Components/Charts/PieChart/PieChartComponent.js
+++ src/components/Reusable Components/Charts/PieChart/PieChartComponent.js
@@ -1,36 +1,38 @@
 import React from 'react';
-import { PieChart, Pie, Cell } from 'recharts';
+import { PieChart, Pie, Cell, Label } from 'recharts';
 
-const PieChartComponent = ({ data, height, width, innerRadius = 50, outerRadius = 60, paddingAngle = 5 }) => {
+const PieChartComponent = ({ data, height, width, innerRadius = 50, outerRadius = 60, paddingAngle = 5, cx = "50%", cy = "50%" }) => {
+  const COLORS = ['#A5381F', '#4B2C20', '#B34729'];
 
+  const percentage = (data[0].value / (data[0].value + data[1].value)) * 100;
 
-    const COLORS = ['#A5381F', '#4B2C20', '#B34729']; // Define your color palette
-
-    console.table(data);
-    
-    return (
-
-            <PieChart width={width} height={height}> {/* Ensure the PieChart has relative width and height */}
-                <Pie 
-                    data={data} 
-                    dataKey="value" 
-                    nameKey="name"
-                    cx="50%"
-                    cy="50%" 
-                    innerRadius={innerRadius} 
-                    outerRadius={outerRadius} 
-                    paddingAngle={paddingAngle}
-                >
-                    {data.map((entry, index) => (
-                        <Cell 
-                            key={`cell-${index}`} 
-                            fill={COLORS[index % COLORS.length]} 
-                        />
-                    ))}
-                </Pie>
-            </PieChart>
-
-    );
-}
+  return (
+    <PieChart width={width} height={height}>
+      <Pie
+        data={data}
+        cx={cx}
+        cy={cy}
+        innerRadius={innerRadius}
+        outerRadius={outerRadius}
+        paddingAngle={paddingAngle}
+      >
+        {data.map((entry, index) => (
+          <Cell
+            key={`cell-${index}`}
+            fill={COLORS[index % COLORS.length]}
+          />
+        ))}
+        
+        {/* Using Recharts' Label to display text at the center */}
+        <Label 
+          value={`${percentage.toFixed(0)}%`} 
+          position="center" 
+          fontSize={24}
+          fill="#333" 
+        />
+      </Pie>
+    </PieChart>
+  );
+};
 
 export default PieChartComponent;
